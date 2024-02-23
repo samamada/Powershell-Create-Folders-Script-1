a0a703cc663cb4e0791a2713a4b112058a467ece
Samuel Anekwe <158786273+samamada@users.noreply.github.com>
2024-02-23 15:38:33 -0700

    Add files via upload

---
+++ PowerShell-Folder-Creation-Script-1.ps1
@@ -0,0 +1,36 @@
+﻿#POWERSHELL PROJECT 1:
+#This Script creates multiple folders using a do loop and variable
+
+#SETTING UP THE VARIABLES
+
+$intfolders = 10
+$intPad = 0
+$i = 1
+New-Variable -Name strPrefix -Value "testfolder" -Option constant
+
+#CREATING THE LOOP STATEMENT
+
+Do {
+    #If condition
+
+    if ($i -lt 10) {
+
+    New-Item -Path C: -Name $strPrefix$intPad$i -Type directory
+    }
+
+    #when Condition is False
+
+    else {
+    New-Item -Path C: -Name $strPrefix$i -Type directory
+    }
+
+    #$i to increment by 1
+
+    $i++
+    }
+
+    #STOPPING THE LOOP
+
+Until ($i -eq $intfolders+1)
+
+#Thank you :)
